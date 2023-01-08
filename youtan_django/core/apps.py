86d7f6e66564d00c444954499c08b23a05b8e0a6
ToledoVitor <usedbr@gmail.com>
2023-01-08 20:37:04 -0300

    Customizing models and starting serializers

--- youtan_django/core/apps.py
+++ youtan_django/core/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    name = 'youtan_django.core'
+    verbose_name = 'core models'
+
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
--- youtan_django/core/models.py
+++ youtan_django/core/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 
-from core.commons import Estados
+from youtan_django.core.commons import Estados
 
 
 class EntidadeFinanceira(models.Model):
@@ -20,17 +20,25 @@ class EntidadeFinanceira(models.Model):
     cep = models.CharField(default="", blank=True, max_length=256)
 
     cidade = models.CharField(default="", blank=True, max_length=256)
-    estado = models.CharField(max_length=2, choices=Estados.Choices)
+    estado = models.CharField(max_length=2, choices=Estados.choices)
 
 
-class Leilao(models.model):
+class Leilao(models.Model):
     # https://docs.djangoproject.com/en/2.1/ref/contrib/contenttypes/#generic-relations
+
     item_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    item_object = GenericForeignKey("content_type", "object_id")
+    item_object = GenericForeignKey("item_type", "item_id")
     # That's the field that holds the other model instance (Imovel or Veiculo) 
     item_id = models.PositiveIntegerField()
     
     minimum_increment = DecimalField(max_digits=12, decimal_places=2, default=D(0))
+
+    # When it get's closed by user or admin    
+    ended = models.BooleanField(default=False)
+    ended_at = models.DateTimeField(blank=True, null=True)
+
+    # When it will automatically ends
+    due_date = models.DateTimeField(blank=True, null=True)
 
     entidade_financeira = models.ForeignKey(EntidadeFinanceira, on_delete=models.CASCADE)
 
@@ -38,7 +46,13 @@ class Leilao(models.model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def get_latest_lance(self):
-        return self.lance_set.order_by("-created_at").first()
+        return (
+            self
+            .lance_set
+            .filter(deleted=False)
+            .order_by("-created_at")
+            .first()
+        )
 
 
 class Lance(models.Model):
@@ -47,18 +61,20 @@ class Lance(models.Model):
 
     money_value = DecimalField(max_digits=12, decimal_places=2, default=D(0))
 
+    # Custom field that represent's if the current vehicle is deleted to receive offers
+    deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class Imovel(models.model):
+class Imovel(models.Model):
     class TipoImovel(models.TextChoices):
       RESIDENCIAL = "residencial"
       COMERCIAL = "comercial"
       RURAL = "rural"
 
     leilao = GenericRelation(Leilao)
-    tipo_imovel = models.CharField(max_length=256, choices=TipoImovel.Choices)
+    tipo_imovel = models.CharField(max_length=256, choices=TipoImovel.choices)
 
     logradouro = models.CharField(default="", blank=True, max_length=256)
     bairro = models.CharField(default="", blank=True, max_length=256)
@@ -66,10 +82,10 @@ class Imovel(models.model):
     cep = models.CharField(default="", blank=True, max_length=256)
 
     cidade = models.CharField(default="", blank=True, max_length=256)
-    estado = models.CharField(max_length=2, choices=Estados.Choices)
+    estado = models.CharField(max_length=2, choices=Estados.choices)
 
-    # Custom field that represent's if the current immobile is avaliable to receive offers
-    avaliable = models.BooleanField(default=True)
+    # Custom field that represent's if the object is valid
+    deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -85,7 +101,7 @@ class Veiculo(models.Model):
 
     placa = models.CharField(default="", max_length=7, blank=True)
 
-    # Custom field that represent's if the current vehicle is avaliable to receive offers
-    avaliable = models.BooleanField(default=True)
+    # Custom field that represent's if the object is valid
+    deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
